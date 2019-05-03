f93e59f1e8bdc8988f0951c436523a8c01ace8d0
HuangHe <huanghe@zuifuli.com>
2019-05-03 21:38:27 +0800

    delete create-react-app unused things,Build a basic template to write components.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,15 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
-
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    rules: [
-      // add your custom rules.
-    ],
-  },
+const path = require("path");
+const TSDocgenPlugin = require("react-docgen-typescript-webpack-plugin");
+module.exports = (baseConfig, env, defaultConfig) => {
+  defaultConfig.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve("awesome-typescript-loader")
+  },{
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  });
+  defaultConfig.plugins.push(new TSDocgenPlugin());
+  defaultConfig.resolve.extensions.push(".ts", ".tsx");
+  return defaultConfig;
 };
